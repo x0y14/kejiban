a3c84753564058c0f9252ce5a387986d84999a70
x0y14 <ya01ma08@gmail.com>
2025-07-06 13:35:37 +0900

    アカウント定義だけ追加

---
+++ database/init/create-table.sql
@@ -0,0 +1,35 @@
+create database if not exists redditclone;
+use redditclone;
+
+create table accounts (
+    id binary(16) not null,
+    email varchar(255) not null,
+    created_at datetime not null default current_timestamp,
+    updated_at datetime not null default current_timestamp on update current_timestamp,
+
+    primary key(id),
+    unique index uq_email(email)
+);
+
+create table credentials (
+    account_id binary(16) not null,
+    hashed_password varchar(255) not null,
+    created_at datetime not null default current_timestamp,
+    updated_at datetime not null default current_timestamp on update current_timestamp,
+
+    primary key(account_id),
+    foreign key(account_id) references accounts(id)
+);
+
+create table profiles (
+    account_id binary(16) not null,
+    display_name varchar(50) not null,
+    bio text not null default "",
+    created_at datetime not null default current_timestamp,
+    updated_at datetime not null default current_timestamp on update current_timestamp,
+
+
+    primary key(account_id),
+    unique index uq_display_name(display_name),
+    foreign key(account_id) references accounts(id)
+);
